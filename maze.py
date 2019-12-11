b8c5f76b9b64bbff11cc2b4a72fc18017462023b
philippeitis <33013301+philippeitis@users.noreply.github.com>
2019-12-11 14:52:55 -0800

    Optimizations to speed up code 2x
    I removed the print statement, as it isn't really necessary. I moved i1 out of the innermost loop, and replaced np.absolute with abs - this speeds the code up by 2x, and the image is exactly the same. 
    I also replaced the enumerate calls with range() calls, as you don't use the rows and columns.

--- maze.py
+++ maze.py
@@ -3,44 +3,49 @@ from pygame import gfxdraw
 import numpy as np
 from noise import pnoise2
 import random
+from timeit import default_timer as timer
 
-pygame.init()
-windowSize = 500
-clock = pygame.time.Clock()
-threshhold = 0.1
-# Open a new window
-size = (windowSize,windowSize)
-screen = pygame.display.set_mode(size)
 
-pygame.display.set_caption("Maze generator")
+if __name__ == "__main__":
+    pygame.init()
+    windowSize = 500
+    clock = pygame.time.Clock()
+    threshhold = 0.1
+    # Open a new window
+    size = (windowSize, windowSize)
+    screen = pygame.display.set_mode(size)
 
-runGame = True
+    pygame.display.set_caption("Maze generator")
 
-pixels = np.ones([windowSize,windowSize])
+    runGame = True
 
-offset_x = random.random() * 10000
-offset_y = random.random() * 10000
+    start = timer()
+    pixels = np.zeros(size)
 
-for i,row in enumerate(pixels):
-    for j,column in enumerate(pixels):
-        i1 = i/10 +offset_x
-        j1 = j/10 +offset_y
-        noise = np.absolute(pnoise2(i1, j1))
-        pixels[i][j] = noise
+    offset_x = random.randint(0, 10000)
+    offset_y = random.randint(0, 10000)
 
-for a in pixels:
-    print(a)
-while runGame:
+    for i in range(windowSize):
+        i1 = i / 10 + offset_x
+        for j in range(windowSize):
+            j1 = j / 10 + offset_y
+            noise = abs(pnoise2(i1, j1))
+            pixels[i][j] = noise
 
-    #handle quit event
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            runGame = False
+    end = timer()
+    print("%f seconds elapsed." % (end - start))
 
-    for i,row in enumerate(pixels):
-        for j,column in enumerate(pixels):
-            if not pixels[i][j] > threshhold:
-                gfxdraw.pixel(screen, i, j, (255,255,255))
+    while runGame:
 
-    pygame.display.flip()
-    clock.tick(60)
+        # handle quit event
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                runGame = False
+
+        for i in range(windowSize):
+            for j in range(windowSize):
+                if not pixels[i][j] > threshhold:
+                    gfxdraw.pixel(screen, i, j, (255, 255, 255))
+
+        pygame.display.flip()
+        clock.tick(60)
