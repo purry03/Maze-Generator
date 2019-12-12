e020c8490f6f058be2b385f70130dec7c29c10a6
philippeitis <33013301+philippeitis@users.noreply.github.com>
2019-12-11 20:44:26 -0800

    Optimizations to speed up code 2x (#1)
    * Optimizations to speed up code 2x
    
    I removed the print statement, as it isn't really necessary. I moved i1 out of the innermost loop, and replaced np.absolute with abs - this speeds the code up by 2x, and the image is exactly the same. 
    I also replaced the enumerate calls with range() calls, as you don't use the rows and columns.
    
    * Move abs() out of inner loop for speed up.
    
    Generally, it's best to do as little as possible inside the innermost loop, so I used numpy's abs function outside of the loop for the same effect.
    
    * Change not > to <=
    
    * Add support for generating PNGs natively.
    
    I've added command line args to allow setting the image file name, dimensions, threshold, and random noise seed.  Additionally, removed dependency on PyGame and replaced it with a dependency on PIL to save and display images.
    
    * Remove extraneous imports, allow threshold and seed to be floats.
    
    * Update README.md
    
    Change requirements.
    
    * Update README.md
    
    * Add large and small sample image with better cropping.
    
    * Update demo images
    
    * Update README.md

--- maze.py
+++ maze.py
@@ -1,46 +1,103 @@
-import pygame
-from pygame import gfxdraw
 import numpy as np
 from noise import pnoise2
 import random
+import argparse
 
-pygame.init()
-windowSize = 500
-clock = pygame.time.Clock()
-threshhold = 0.1
-# Open a new window
-size = (windowSize,windowSize)
-screen = pygame.display.set_mode(size)
 
-pygame.display.set_caption("Maze generator")
+def make_png(pixels):
+    from PIL import Image
 
-runGame = True
+    im = Image.new('RGBA', size, 0)
+    im_pixels = im.load()
+    im_arr = np.zeros(size).astype("ubyte")
+    im_arr[pixels <= threshold] = 255
+    for x in range(size[0]):
+        for y in range(size[1]):
+            im_pixels[x, y] = (im_arr[x, y], im_arr[x, y], im_arr[x, y])
+    return im
 
-pixels = np.ones([windowSize,windowSize])
 
-offset_x = random.random() * 10000
-offset_y = random.random() * 10000
+def save_png(file_name: str, im) -> None:
+    import os
 
-for i,row in enumerate(pixels):
-    for j,column in enumerate(pixels):
-        i1 = i/10 +offset_x
-        j1 = j/10 +offset_y
-        noise = np.absolute(pnoise2(i1, j1))
-        pixels[i][j] = noise
+    if os.path.splitext(file_name)[-1] != ".png":
+        file_name += ".png"
+        print("save_png: Automatically appended .png to file_name")
+    im.save(file_name, 'PNG')
 
-for a in pixels:
-    print(a)
-while runGame:
 
-    #handle quit event
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            runGame = False
+def make_im_from_arr(image_arr):
+    image_arr[image_arr < threshold] = 255
+    image_arr[image_arr >= threshold] = 0
+    image_arr = image_arr.astype(np.ubyte)
+    white_pixels = np.dstack((image_arr, image_arr, image_arr))
+    print(white_pixels.shape)
+    return pygame.surfarray.make_surface(np.dstack(white_pixels))
 
-    for i,row in enumerate(pixels):
-        for j,column in enumerate(pixels):
-            if not pixels[i][j] > threshhold:
-                gfxdraw.pixel(screen, i, j, (255,255,255))
 
-    pygame.display.flip()
-    clock.tick(60)
+def generate_noise_array(size):
+    pixels = np.zeros(size)
+
+    offset_x = random.randint(0, 10000)
+    offset_y = random.randint(0, 10000)
+
+    for i in range(size[0]):
+        i1 = i / 10 + offset_x
+        for j in range(size[1]):
+            j1 = j / 10 + offset_y
+            pixels[i][j] = pnoise2(i1, j1)
+    np.abs(pixels, out=pixels)
+    return pixels
+
+
+def run_pygame():
+    pygame.init()
+    clock = pygame.time.Clock()
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption("Maze generator")
+
+    runGame = True
+
+    while runGame:
+
+        # handle quit event
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                runGame = False
+
+        for i in range(size[0]):
+            for j in range(size[1]):
+                if pixels[i][j] <= threshold:
+                    gfxdraw.pixel(screen, i, j, (255, 255, 255))
+
+        pygame.display.flip()
+        clock.tick(60)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generate perlin noise mazes")
+    parser.add_argument('file_name', type=str, default=None, nargs='?',
+                        help='Output filename.')
+    parser.add_argument('-im_w', dest='width', type=int, default=500,
+                        help='Set the image width')
+    parser.add_argument('-im_h', dest='height', type=int, default=500,
+                        help='Set the image height')
+    parser.add_argument('-t', dest='threshold', type=float, default=0.1,
+                        help='Set the perlin noise threshold.')
+    parser.add_argument('-s', dest='seed', type=float, default=random.random(),
+                        help='Provide a seed to generate perlin maps.')
+
+    args = parser.parse_args()
+
+    random.seed(args.seed)
+    threshold = args.threshold
+    size = (args.width, args.height)
+    pixels = generate_noise_array(size)
+
+    if args.file_name:
+        save_png(args.file_name, make_png(pixels))
+    else:
+        im = make_png(pixels)
+        im.show()
+        file_name = str(args.seed).replace(".", "_") + ".png"
+        save_png(file_name, make_png(pixels))
