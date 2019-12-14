0fb81788852d90d9204870e3e53111ebb9c14ade
philippeitis <33013301+philippeitis@users.noreply.github.com>
2019-12-13 21:03:24 -0800

    Add scrolling to maze.py (#2)
    * Add scrolling to maze.py
    
    This adds a semi-functional scrolling feature. However, once you scroll past the generated image, the lines become much thicker for no apparent reason. However, for the first few seconds, everything works perfectly.
    
    * Update maze.py
    
    * Tweak random noise, refactoring.
    
    Renamed pixels to noise_map where relevant, as it more accurately reflects what happens. Additionally, updated randint boundaries to a large number.
    
    * Forgot to use absolute values.
    
    Turns out the big lines were due to me not using absolute values. Code now works correctly.

--- maze.py
+++ maze.py
@@ -4,12 +4,20 @@ import random
 import argparse
 
 
+def get_big_nums():
+    big_num = 2 ** 16
+    offset_x = random.randint(-big_num, big_num)
+    offset_y = random.randint(-big_num, big_num)
+    return offset_x, offset_y
+
+
 def make_png(pixels):
     from PIL import Image
 
+    size = pixels.shape
     im = Image.new('RGBA', size, 0)
     im_pixels = im.load()
-    im_arr = np.zeros(size).astype("ubyte")
+    im_arr = np.zeros(size).astype(np.ubyte)
     im_arr[pixels <= threshold] = 255
     for x in range(size[0]):
         for y in range(size[1]):
@@ -26,58 +34,134 @@ def save_png(file_name: str, im) -> None:
     im.save(file_name, 'PNG')
 
 
-def make_im_from_arr(image_arr):
-    image_arr[image_arr < threshold] = 255
-    image_arr[image_arr >= threshold] = 0
-    image_arr = image_arr.astype(np.ubyte)
-    white_pixels = np.dstack((image_arr, image_arr, image_arr))
-    print(white_pixels.shape)
-    return pygame.surfarray.make_surface(np.dstack(white_pixels))
+def make_im_from_arr(noise_map, screen_array):
+    import pygame
+    screen_array[:, :, 0][noise_map < threshold] = 255
+    screen_array[:, :, 0][noise_map >= threshold] = 0
+    screen_array[:, :, 1] = screen_array[:, :, 0]
+    screen_array[:, :, 2] = screen_array[:, :, 0]
+    return pygame.surfarray.make_surface(screen_array)
+
+
+def update_noise_map(noise_map, row_index, true_index, offset_x, offset_y):
+    # Needs to replace a row with the correct offset.
+    # To do this, we need the row / column.
+    shape = noise_map.shape
+    size = (shape[0] // 2, shape[1] // 2)
+
+    if row_index == size[0]:
+        # Update whole lower matrix.
+        for i in range(size[0], size[0] * 2):
+            i1 = (i + true_index) / 10 + offset_x
+            for j in range(size[1], size[1] * 2):
+                j1 = (j + true_index) / 10 + offset_y
+                noise_map[i][j] = pnoise2(i1, j1)
+
+        np.abs(noise_map[size[0]:size[0] * 2, size[1]:size[1] * 2],
+               out=noise_map[size[0]:size[0] * 2, size[1]:size[1] * 2])
+
+        return true_index + size[0]
+    else:
+        y = (row_index + true_index) / 10 + offset_y
+        for j in range(row_index, size[0] + row_index):
+            x = (j + true_index) / 10 + offset_x
+            noise_map[j][row_index] = pnoise2(x, y)
+        x = (row_index + true_index) / 10 + offset_x
+        for j in range(row_index, size[0] + row_index):
+            y = (j + true_index) / 10 + offset_y
+            noise_map[row_index][j] = pnoise2(x, y)
+
+        np.abs(noise_map[row_index, row_index: size[0] + row_index],
+               out=noise_map[row_index, row_index: size[0] + row_index])
+        np.abs(noise_map[row_index: size[0] + row_index, row_index],
+               out=noise_map[row_index: size[0] + row_index, row_index])
+        return true_index
+
+
+def update_matrix_funky(noise_map, row_index, true_index, offset_x, offset_y):
+    # Needs to replace a row with the correct offset.
+    # To do this, we need the row / column.
+    if row_index == noise_map.shape[0] // 2:
+        # Update whole lower matrix.
+        for i in range(size[0], size[0] * 2):
+            i1 = i / 10 + offset_x
+            for j in range(size[1], size[1] * 2):
+                j1 = j / 10 + offset_y
+                noise_map[i][j] = pnoise2(i1, j1)
+        np.abs(noise_map[size[0]:size[0] * 2, size[1]:size[1] * 2],
+               out=noise_map[size[0]:size[0] * 2, size[1]:size[1] * 2])
+    else:
+        for j in range(row_index, size[0] + row_index):
+            x = j / 10 + offset_x
+            noise_map[j][row_index] = pnoise2(x, offset_y)
+        np.abs(noise_map[j, row_index: size[0] + row_index], out=noise_map[j, row_index: size[0] + row_index])
+        for j in range(row_index, size[0] + row_index):
+            y = j / 10 + offset_y
+            noise_map[row_index][j] = pnoise2(offset_x, y)
+        np.abs(noise_map[row_index: size[0] + row_index, j], out=noise_map[row_index: size[0] + row_index, j])
+
+    return true_index + 1
+
+
+def generate_diagonal_noise_array(size):
+    noise_map = np.zeros((size[0] * 2, size[1] * 2))
+    offset_x, offset_y = get_big_nums()
+    true_index = 0
+    for i in range(size[0] + 1):
+        true_index = update_noise_map(noise_map, i, true_index, offset_x, offset_y)
+    np.abs(noise_map, out=noise_map)
+    return noise_map, offset_x, offset_y, true_index
 
 
 def generate_noise_array(size):
-    pixels = np.zeros(size)
+    noise_map = np.zeros(size)
 
-    offset_x = random.randint(0, 10000)
-    offset_y = random.randint(0, 10000)
+    offset_x, offset_y = get_big_nums()
 
     for i in range(size[0]):
         i1 = i / 10 + offset_x
         for j in range(size[1]):
             j1 = j / 10 + offset_y
-            pixels[i][j] = pnoise2(i1, j1)
-    np.abs(pixels, out=pixels)
-    return pixels
+            noise_map[i][j] = pnoise2(i1, j1)
+    np.abs(noise_map, out=noise_map)
+    return noise_map
 
 
-def run_pygame():
+def run_pygame(size):
+    import pygame
     pygame.init()
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption("Maze generator")
-
-    runGame = True
-
-    while runGame:
-
+    noise_map, offset_x, offset_y, true_index = generate_diagonal_noise_array(size)
+    screen_array = np.zeros((size[0], size[1], 3), np.ubyte)
+    run_game = True
+    i = 0
+    while run_game:
         # handle quit event
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                runGame = False
+                run_game = False
 
-        for i in range(size[0]):
-            for j in range(size[1]):
-                if pixels[i][j] <= threshold:
-                    gfxdraw.pixel(screen, i, j, (255, 255, 255))
+        image = make_im_from_arr(noise_map[i:i + size[0], i:i + size[0]], screen_array)
+        # save_png("surf%i.png" % (i + true_index), make_png(noise_map))
+        screen.blit(image, (0, 0))
+        pygame.display.update()
+        true_index = update_noise_map(noise_map, i, true_index, offset_x, offset_y)
+        i += 1
+        if i == size[0] + 1:
+            i = 1
 
-        pygame.display.flip()
         clock.tick(60)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate perlin noise mazes")
-    parser.add_argument('file_name', type=str, default=None, nargs='?',
-                        help='Output filename.')
+    image_or_scrolling = parser.add_mutually_exclusive_group()
+    image_or_scrolling.add_argument('--scroll', action='store_true',
+                                    help="Run a scrolling image")
+    image_or_scrolling.add_argument('file_name', type=str, default=None, nargs='?',
+                                    help='Output filename.')
     parser.add_argument('-im_w', dest='width', type=int, default=500,
                         help='Set the image width')
     parser.add_argument('-im_h', dest='height', type=int, default=500,
@@ -88,16 +172,18 @@ if __name__ == "__main__":
                         help='Provide a seed to generate perlin maps.')
 
     args = parser.parse_args()
-
     random.seed(args.seed)
     threshold = args.threshold
     size = (args.width, args.height)
-    pixels = generate_noise_array(size)
-
-    if args.file_name:
-        save_png(args.file_name, make_png(pixels))
+    if args.scroll:
+        run_pygame(size)
     else:
-        im = make_png(pixels)
-        im.show()
-        file_name = str(args.seed).replace(".", "_") + ".png"
-        save_png(file_name, make_png(pixels))
+        noise_map = generate_noise_array(size)
+
+        if args.file_name:
+            save_png(args.file_name, make_png(noise_map))
+        else:
+            im = make_png(noise_map)
+            im.show()
+            file_name = str(args.seed).replace(".", "_") + ".png"
+            save_png(file_name, make_png(noise_map))
